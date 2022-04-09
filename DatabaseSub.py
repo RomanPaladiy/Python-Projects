709bb9e5b9e034ba446fd7d975ddc12194c796ce
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-04-09 16:52:28 -0700

    Add files via upload

--- DatabaseSub.py
+++ DatabaseSub.py
@@ -19,27 +19,24 @@ conn.close()
 #restating that I am making another connection to the db to input values
 conn = sqlite3.connect('submission.db')
 
-#inputting values into column.
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_submission(col_file) VALUES (?)", \
-                ('Hello.txt',))
-    cur.execute("INSERT INTO tbl_submission(col_file) VALUES (?)", \
-                ('World.txt',))
-    conn.commit()
+
+# this is the list of files to be parsed through
+
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+
+
+# this is a loop through each object in the tuple to find the file names that end in .txt.
+for x in fileList:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_submission(col_file) VALUES (?)",(x,))
+            print(x)
+
 conn.close()
+        
 
-# calling another connection for changes to be made 
-conn = sqlite3.connect('submission.db')
-
-
-with conn:
-    cur = conn.cursor()
-    cur.execute(" SELECT * FROM tbl_submission")
-    varFile = cur.fetchall()
-    for item in varFile:
-        msg = "File Name: {}".format(item[1])
-        print(msg)
 
 
 
