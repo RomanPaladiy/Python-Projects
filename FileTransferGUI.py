fb0e126c190a5cc89bee7311558f1299aba41665
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-04-19 15:35:40 -0700

    Add files via upload

--- FileTransferGUI.py
+++ FileTransferGUI.py
@@ -5,6 +5,29 @@ from tkinter import *
 from tkinter import  filedialog
 import shutil
 import os
+import time
+import datetime
+
+
+SECONDS_IN_DAY = 24 * 60 * 60
+now = time.time()
+before = now - SECONDS_IN_DAY
+
+def last_mod_time(file):
+    return os.path.getmtime(file)
+
+
+# File Transfer
+def move_files():
+        source = sourceDir.get()
+        destination = destinationDir.get()
+        files = os.listdir(source)
+
+# This is saying move the files represented by "i" to their destination
+        for i in files:
+                src_file = os.path.join(source, i)
+                if last_mod_time(src_file) > before:
+                        shutil.move(source + '/' + i, destination)
 
 def source_directory():
         dirName = filedialog.askdirectory()
@@ -16,7 +39,6 @@ def destination_directory():
         dirName = filedialog.askdirectory()
         destinationDir.delete(0,END)
         destinationDir.insert(0,dirName)
-
 
 m = tkinter.Tk()
 
@@ -42,26 +64,8 @@ button2.grid(row=0, column=0,padx=(20,10),pady=(100,0))
 destinationDir = Entry(m,width=100)
 destinationDir.grid(row=0, column=1, padx=20,pady=(100,0), sticky=E)
 
-
-# File Transfer 
-source = sourceDir
-destination = destinationDir
-
-files = os.listdir(path)
-
-for i in files:
-        # this is saying move the files represented by 'i' to their destination
-        shutil.move(source+i, destination)
-
-m.mainloop()
+# Creating the third button which allows the files to be transferred.
+button3= Button(m, text= "Move Files", padx=20, command=move_files)
+button3.grid(row=0, column=0, padx=(20,10),pady=(200,0))
 
         
-
-
-
-
-
-
-
-
-
